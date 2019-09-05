d9aff26bb8bc81b3aed8c060868a6dfd224aa311
Oleksii Asiutin <oleksii.asiutin@thredup.com>
2019-09-05 12:54:39 +0300

    add env config to grab response body and a status code option

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const httpProxy = require('http-proxy');
 const TARGET_HOST = process.env.TARGET_HOST || "http://127.0.0.1";
 const TARGET_PORT = process.env.TARGET_PORT || 8000
 const LISTEN_PORT = process.env.LISTEN_PORT || 3000
-const GRAB_REQ_BODY = process.env.GRAB_REQ_BODY || false;
+const GRAB_REQUEST_BODY = process.env.GRAB_REQUEST_BODY || false;
+const GRAB_RESPONSE_BODY = process.env.GRAB_RESPONSE_BODY || false;
+const STATUS_CODE_LEVEL = +process.env.STATUS_CODE_LEVEL || 500
 
 var proxy = httpProxy.createProxyServer({
   target:`${TARGET_HOST}:${TARGET_PORT}`
 }).listen(LISTEN_PORT);
 
-if (GRAB_REQ_BODY) {
+if (GRAB_REQUEST_BODY) {
   proxy.on('proxyReq', (proxyReq, req, res, options) => {
 
     let body = ""
@@ -23,17 +25,34 @@ if (GRAB_REQ_BODY) {
 
 proxy.on('proxyRes', (proxyRes, req, res) => {
 
-  if (proxyRes.statusCode >= 500) {
+  if (proxyRes.statusCode >= STATUS_CODE_LEVEL) {
 
-    console.log(JSON.stringify({
-      res_statusCode: proxyRes.statusCode,
-      res_statusMessage: proxyRes.statusMessage,
-      res_headers: proxyRes.headers,
-      req_headers: req.headers,
-      req_url: req.url,
-      req_method: req.method,
-      req_body: req.body
-    }))
+      let resultLoggingObj = {
+        res_statusCode: proxyRes.statusCode,
+        res_statusMessage: proxyRes.statusMessage,
+        res_headers: proxyRes.headers,
+        req_headers: req.headers,
+        req_url: req.url,
+        req_method: req.method,
+        req_body: req.body
+      }
+
+
+    if (GRAB_RESPONSE_BODY) {
+      let body = new Buffer('');
+        proxyRes.on('data', function (data) {
+            body = Buffer.concat([body, data]);
+        });
+        proxyRes.on('end', function () {
+            body = body.toString();
+            resultLoggingObj.res_body = body
+            console.log(JSON.stringify(resultLoggingObj))
+        });
+    } else {
+      console.log(JSON.stringify(resultLoggingObj))
+    }
+
+    
   }
 
   proxyRes.pipe(res)
